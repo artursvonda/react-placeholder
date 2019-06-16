2bee1d5d57ff98dd9e16a976360e148bd85b9dd7
Arturs Vonda <arturs@artursvonda.lv>
2019-06-16 18:08:14 +0300

    Added more knobs to Paragraph Storybook

--- stories/ParagraphPlaceholder.stories.tsx
+++ stories/ParagraphPlaceholder.stories.tsx
@@ -1,14 +1,20 @@
+import { color, number, text, array } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { defaultColor } from '../src/_contants';
 import ParagraphPlaceholder from '../src/ParagraphPlaceholder';
 
-import { number, color } from '@storybook/addon-knobs';
-
 const stories = storiesOf('Components', module);
 
 stories.add(
     'ParagraphPlaceholder',
-    () => <ParagraphPlaceholder rows={number('rows', 3)} color={color('color', defaultColor)} />,
+    () => (
+        <ParagraphPlaceholder
+            rows={number('rows', 3)}
+            color={color('color', defaultColor)}
+            lineSpacing={text('lineSpacing', '0.7em')}
+            widths={array('widths', [97, 100, 94, 90, 98, 95, 98, 40])}
+        />
+    ),
     { info: { inline: true } },
 );
